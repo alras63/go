4f805c67b919a290ace67b0a39d58b43d52922e4
no-bash <blunskay@gmail.com>
2021-05-29 15:37:35 +0300

    fix

--- src/pages/GameBoard/components/Help/Help.js
+++ src/pages/GameBoard/components/Help/Help.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Players from "../GameInfo/components/Players/Players";
-import TabsHelp from '../ButtonsBoard/ButtonsBoard'
+import TabsHelp from "../ButtonsBoard/ButtonsBoard";
 import {
   HEATMAP_FULL,
   HEATMAP_ZONE_QUARTER,
@@ -10,8 +10,7 @@ import {
 import { laguageVariation } from "../../../../language";
 import { isMobile } from "react-device-detect";
 /*Отвечает за тултипы */
-import ReactTooltip from 'react-tooltip';
-
+import ReactTooltip from "react-tooltip";
 
 const Wrapper = styled.div`
   width: 90%;
@@ -38,22 +37,22 @@ const HelpItem = styled.div`
 `;
 
 const Help = ({
-    enemyPass,
-    stepColor,
-    yourColor,
-    you,
-    opponent,
-    stepMain,
-    stepTwo,
-    handleHelp,
-    activeHelpId,
-    scores,
-    times,
-    hint,
-    setHint,
-    setResign,
-    logsHint
-  }) => {
+  enemyPass,
+  stepColor,
+  yourColor,
+  you,
+  opponent,
+  stepMain,
+  stepTwo,
+  handleHelp,
+  activeHelpId,
+  scores,
+  times,
+  hint,
+  setHint,
+  setResign,
+  logsHint,
+}) => {
   return (
     <Wrapper>
       <Players
@@ -66,136 +65,236 @@ const Help = ({
         stepTwo={stepTwo}
         times={times}
       />
-       <TabsHelp hint={hint} setHint={setHint} setResign={setResign}/>
+      <TabsHelp hint={hint} setHint={setHint} setResign={setResign} />
       <HelpWrapper>
         {/*Для тултипа у кнопки указывается data-tip и data-for */}
-      <HelpItem
-          data-tip 
-          data-for={laguageVariation['CaptureOfTheInitialTerritory']}
+        <HelpItem
+          data-tip
+          data-for={laguageVariation["CaptureOfTheInitialTerritory"]}
           active={activeHelpId === 22}
           onClick={(e) =>
-            scores && logsHint(e) || handleHelp({ type: "map", id: 22 })
+            (scores && logsHint(e)) || handleHelp({ type: "map", id: 22 })
           }
         >
-          {laguageVariation['CaptureOfTheInitialTerritory']}
+          {laguageVariation["CaptureOfTheInitialTerritory"]}
         </HelpItem>
-        
+
         {/*Сам тултип указывается id как и data-for у кнопки. Внутри компонента тултипа указывается описание */}
 
-        <ReactTooltip id={laguageVariation['CaptureOfTheInitialTerritory']} type='success'>
-          Описание сценария/Подсказки. Мультиязычное!
+        <ReactTooltip
+          id={laguageVariation["CaptureOfTheInitialTerritory"]}
+          type="success"
+        >
+          scenarii
         </ReactTooltip>
 
         <HelpItem
+          data-tip
+          data-for={laguageVariation["BestMove"]}
           active={activeHelpId === 1}
           onClick={(e) =>
-            scores && logsHint(e) || handleHelp({ type: "single", id: 1, count: 1 })
+            (scores && logsHint(e)) ||
+            handleHelp({ type: "single", id: 1, count: 1 })
           }
         >
-          {laguageVariation['BestMove']}
+          {laguageVariation["BestMove"]}
         </HelpItem>
-		
-		<HelpItem
+        <ReactTooltip
+          id={laguageVariation["BestMove"]}
+          type="success"
+        >
+          {laguageVariation['BestmoveDes']}
+        </ReactTooltip>
+
+        <HelpItem
+          data-tip
+          data-for={laguageVariation["GameDevelopment4"]}
           active={activeHelpId === 31}
           onClick={(e) =>
-            scores && logsHint(e) || handleHelp({ type: "futuredGame", id: 31, count: 4 })
+            (scores && logsHint(e)) ||
+            handleHelp({ type: "futuredGame", id: 31, count: 4 })
           }
         >
-          {laguageVariation['GameDevelopment4']}
+          {laguageVariation["GameDevelopment4"]}
         </HelpItem>
-		
-		
-		<HelpItem
+        <ReactTooltip
+          id={laguageVariation["GameDevelopment4"]}
+          type="success"
+        >
+          {laguageVariation['GameDevelopment4Des']}
+        </ReactTooltip>
+
+        <HelpItem
+          data-tip
+          data-for={laguageVariation["TheOpponentsBestMove"]}
           active={activeHelpId === 2}
           onClick={(e) =>
-            scores && logsHint(e) || handleHelp({ type: "single_war", id: 2, count: 1 })
+            (scores && logsHint(e)) ||
+            handleHelp({ type: "single_war", id: 2, count: 1 })
           }
         >
-          {laguageVariation['TheOpponentsBestMove']}
+          {laguageVariation["TheOpponentsBestMove"]}
         </HelpItem>
+        <ReactTooltip
+          id={laguageVariation["TheOpponentsBestMove"]}
+          type="success"
+        >
+          {laguageVariation['TheOpponentsBestMoveDes']}
+        </ReactTooltip>
         <HelpItem
+          data-tip
+          data-for={laguageVariation["HeatmapDetailed"]}
           active={activeHelpId === HEATMAP_FULL}
           onClick={(e) =>
-            scores && logsHint(e) || handleHelp({ type: "map", id: HEATMAP_FULL })
+            (scores && logsHint(e)) ||
+            handleHelp({ type: "map", id: HEATMAP_FULL })
           }
         >
-          {laguageVariation['HeatmapDetailed']}
+          {laguageVariation["HeatmapDetailed"]}
         </HelpItem>
+        <ReactTooltip
+          id={laguageVariation["HeatmapDetailed"]}
+          type="success"
+        >
+          {laguageVariation['HeatmapDetailedDes']}
+        </ReactTooltip>
         <HelpItem
+          data-tip
+          data-for={laguageVariation["Showbest3"]}
           active={activeHelpId === 16}
           onClick={(e) =>
-            scores && logsHint(e) ||
+            (scores && logsHint(e)) ||
             handleHelp({ type: "multiple", multipleHandleCount: 4, id: 16 })
           }
         >
-          {laguageVariation['Showbest3']}
+          {laguageVariation["Showbest3"]}
         </HelpItem>
-		
-		
+        <ReactTooltip
+          id={laguageVariation["Showbest3"]}
+          type="success"
+        >
+          {laguageVariation['Showbest3Des']}
+        </ReactTooltip>
         <HelpItem
+          data-tip
+          data-for={laguageVariation["WhichQuarter"]}
           active={activeHelpId === HEATMAP_ZONE_QUARTER}
           onClick={(e) =>
-            scores && logsHint(e) || handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER })
+            (scores && logsHint(e)) ||
+            handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER })
           }
         >
-          {laguageVariation['WhichQuarter']}
+          {laguageVariation["WhichQuarter"]}
         </HelpItem>
-		<HelpItem
+        <ReactTooltip
+          id={laguageVariation["WhichQuarter"]}
+          type="success"
+        >
+          {laguageVariation['WhichQuarterDes']}
+        </ReactTooltip>
+        <HelpItem
+          data-tip
+          data-for={laguageVariation["TheBetterHalf"]}
           active={activeHelpId === 24}
           onClick={(e) =>
-            scores && logsHint(e) || handleHelp({ type: "map", id: 24 })
+            (scores && logsHint(e)) || handleHelp({ type: "map", id: 24 })
           }
         >
-          {laguageVariation['TheBetterHalf']}
+          {laguageVariation["TheBetterHalf"]}
         </HelpItem>
-		
-		<HelpItem
+        <ReactTooltip
+          id={laguageVariation["TheBetterHalf"]}
+          type="success"
+        >
+          {laguageVariation['TheBetterHalfDes']}
+        </ReactTooltip>
+        <HelpItem
+          data-tip
+          data-for={laguageVariation["Heatmap1"]}
           active={activeHelpId === HEATMAP_ZONE_QUARTER_ONE}
           onClick={(e) =>
-			scores && logsHint(e) || handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER_ONE, count: 1 })
+            (scores && logsHint(e)) ||
+            handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER_ONE, count: 1 })
           }
         >
-          {laguageVariation['Heatmap1']}
+          {laguageVariation["Heatmap1"]}
         </HelpItem>
-		
-		<HelpItem
+        <ReactTooltip
+          id={laguageVariation["Heatmap1"]}
+          type="success"
+        >
+          {laguageVariation['Heatmap1Des']}
+        </ReactTooltip>
+        <HelpItem
+          data-tip
+          data-for={laguageVariation["Heatmap2"]}
           active={activeHelpId === 3}
           onClick={(e) =>
-			scores && logsHint(e) || handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER_ONE, count: 2 })
+            (scores && logsHint(e)) ||
+            handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER_ONE, count: 2 })
           }
         >
-          {laguageVariation['Heatmap2']}
+          {laguageVariation["Heatmap2"]}
         </HelpItem>
-		<HelpItem
+        <ReactTooltip
+          id={laguageVariation["Heatmap2"]}
+          type="success"
+        >
+           {laguageVariation['Heatmap2Des']}
+        </ReactTooltip>
+        <HelpItem
+          data-tip
+          data-for={laguageVariation["Heatmap3"]}
           active={activeHelpId === 4}
           onClick={(e) =>
-			scores && logsHint(e) || handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER_ONE, count: 3 })
+            (scores && logsHint(e)) ||
+            handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER_ONE, count: 3 })
           }
         >
-          {laguageVariation['Heatmap3']}
+          {laguageVariation["Heatmap3"]}
         </HelpItem>
-		<HelpItem
+        <ReactTooltip
+          id={laguageVariation["Heatmap3"]}
+          type="success"
+        >
+           {laguageVariation['Heatmap3Des']}
+        </ReactTooltip>
+        <HelpItem
+          data-tip
+          data-for={laguageVariation["Heatmap4"]}
           active={activeHelpId === 5}
           onClick={(e) =>
-			scores && logsHint(e) || handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER_ONE, count: 4 })
+            (scores && logsHint(e)) ||
+            handleHelp({ type: "map", id: HEATMAP_ZONE_QUARTER_ONE, count: 4 })
           }
         >
-          {laguageVariation['Heatmap4']}
+          {laguageVariation["Heatmap4"]}
         </HelpItem>
-		
-		
-        <HelpItem
-          active={activeHelpId === 34}
-          onClick={(e) => scores && logsHint(e) || handleHelp({ type: "score", id: 34 })}
+        <ReactTooltip
+          id={laguageVariation["Heatmap4"]}
+          type="success"
         >
-         {laguageVariation['WhoWinning']}
+           {laguageVariation['Heatmap4Des']}
+        </ReactTooltip>
+        <HelpItem
+          data-tip
+          data-for={laguageVariation["WhoWinning"]}
+          active={activeHelpId === 34}
+          onClick={(e) =>
+            (scores && logsHint(e)) || handleHelp({ type: "score", id: 34 })
+          }
+        >
+          {laguageVariation["WhoWinning"]}
         </HelpItem>
+        <ReactTooltip
+          id={laguageVariation["WhoWinning"]}
+          type="success"
+        >
+          {laguageVariation['WhoWinningDes']}
+        </ReactTooltip>
       </HelpWrapper>
     </Wrapper>
   );
 };
-
-
-
 
 export default Help;
